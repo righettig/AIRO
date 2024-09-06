12d8ec4303accbe72079578c04cbded891fd5543
Giacomo Righetti <righetti.giacomo@live.com>
2024-09-06 12:13:12 +0200

    Added auth microservice

---
+++ back-end/microservices/airo-auth-microservice/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace airo_auth_microservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
