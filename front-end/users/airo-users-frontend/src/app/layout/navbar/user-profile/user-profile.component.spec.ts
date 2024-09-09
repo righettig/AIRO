5e19bb5531fcac8546be14a2f1c141690fef5367
Giacomo Righetti <righetti.giacomo@live.com>
2024-09-09 12:00:28 +0200

    Fix unit tests

--- front-end/users/airo-users-frontend/src/app/layout/navbar/user-profile/user-profile.component.spec.ts
+++ front-end/users/airo-users-frontend/src/app/layout/navbar/user-profile/user-profile.component.spec.ts
@@ -1,11 +1,17 @@
-import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { MatMenuHarness } from '@angular/material/menu/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { UserProfileComponent } from './user-profile.component';
+import { TitleCasePipe } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../auth/services/auth.service';
-import { TitleCasePipe } from '@angular/common';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+let loader: HarnessLoader;
 
 describe('UserProfileComponent', () => {
   let component: UserProfileComponent;
@@ -23,18 +29,22 @@ describe('UserProfileComponent', () => {
         MatMenuModule,
         MatButtonModule,
         TitleCasePipe,
-        UserProfileComponent
+        UserProfileComponent,
+        NoopAnimationsModule
       ],
       providers: [
         { provide: AuthService, useValue: authServiceSpy },
         { provide: Router, useValue: routerSpy }
       ]
-    }).compileComponents();
+    })
+      .compileComponents();
 
     fixture = TestBed.createComponent(UserProfileComponent);
     component = fixture.componentInstance;
     component.accountType = 'free';
     fixture.detectChanges();
+
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create', () => {
@@ -46,19 +56,32 @@ describe('UserProfileComponent', () => {
     expect(accountTypeElement.textContent).toBe('Free');
   });
 
-  it('should display profile menu items', () => {
-    const menuItems = fixture.nativeElement.querySelectorAll('button[mat-menu-item]');
-    expect(menuItems.length).toBe(3);
-    expect(menuItems[0].textContent).toContain('Profile');
-    expect(menuItems[1].textContent).toContain('Settings');
-    expect(menuItems[2].textContent).toContain('Logout');
+  it('should render the user profile menu items', async () => {
+    const profileMenu = await loader.getHarness(MatMenuHarness);
+    await profileMenu.open();
+    expect(await profileMenu.isOpen()).toBe(true);
+
+    const items = await profileMenu.getItems();
+    expect(items.length).toBe(3);
+
+    const menuTexts = await Promise.all(
+      items.map(async (item) => {
+        var el = TestbedHarnessEnvironment.getNativeElement(await item.host());
+        return el.querySelector('span')?.innerText;
+      }));
+
+    expect(menuTexts).toEqual(['Profile', 'Settings', 'Logout']);
   });
 
-  it('should call authService.logout and navigate to /login when onLogout is called', fakeAsync(() => {
+  it('should call authService.logout and navigate to /login when onLogout is called', fakeAsync(async () => {
     authServiceSpy.logout.and.returnValue(Promise.resolve());
 
-    const logoutButton = fixture.nativeElement.querySelector('button[mat-menu-item]:last-child');
-    logoutButton.click();
+    const profileMenu = await loader.getHarness(MatMenuHarness);
+    await profileMenu.open();
+    
+    const logout = (await profileMenu.getItems())[2];
+    logout.click();
+    
     tick(); // Simulate passage of time for promises
 
     expect(authServiceSpy.logout).toHaveBeenCalled();
