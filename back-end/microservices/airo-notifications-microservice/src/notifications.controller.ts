08eddef2bdcb97424e5c0b32eac1010bad999ff1
Giacomo Righetti <righetti.giacomo@live.com>
2024-09-09 18:54:54 +0200

    Notifications microservice can now send emails

--- back-end/microservices/airo-notifications-microservice/src/notifications.controller.ts
+++ back-end/microservices/airo-notifications-microservice/src/notifications.controller.ts
@@ -1,13 +1,23 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { Logger } from '@nestjs/common';
+import { EmailService } from './email/email.service';
 
 @Controller()
 export class NotificationsController {
     private logger = new Logger('NotificationsController');
 
+    constructor(private readonly emailService: EmailService) { }
+
     @MessagePattern('user.created')
-    executeAction(email: string) {
+    async executeAction(email: string) {
         this.logger.log('user.created: ', email);
+
+        await this.emailService.sendEmail(
+            email, 
+            "AIRO new account", 
+            "Welcome to AIRO!", 
+            `Welcome to AIRO <strong>${email}</strong>. Head over to <a href="www.airo.ai">AIRO</a> and complete your profile.`
+        );
     }
 }
