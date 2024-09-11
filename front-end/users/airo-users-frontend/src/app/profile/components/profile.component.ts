c7f112cde6bf56321e5dd35031d93b0f7893908a
Giacomo Righetti <righetti.giacomo@live.com>
2024-09-11 22:23:03 +0200

    Improved visuals

--- front-end/users/airo-users-frontend/src/app/profile/components/profile.component.ts
+++ front-end/users/airo-users-frontend/src/app/profile/components/profile.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { ProfileGeneralComponent } from "./profile-general/profile-general.component";
 import { ProfileMembershipComponent } from './profile-membership/profile-membership.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
-  imports: [ProfileGeneralComponent, ProfileMembershipComponent]
+  imports: [
+    MatCardModule,
+    ProfileGeneralComponent, 
+    ProfileMembershipComponent
+  ]
 })
 export class ProfileComponent {
 
