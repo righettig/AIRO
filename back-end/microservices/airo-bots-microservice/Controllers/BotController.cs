537e6198cf0171f5f55aca3c5e269ffc74fb5daf
Giacomo Righetti <righetti.giacomo@live.com>
2024-09-12 21:57:44 +0200

    Created project

---
+++ back-end/microservices/airo-bots-microservice/Controllers/BotController.cs
@@ -0,0 +1,33 @@
+﻿namespace airo_bots_microservice.Controllers;
+
+//using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BotController : ControllerBase
+{
+    //private readonly IMediator _mediator;
+
+    //public BotController(IMediator mediator)
+    public BotController()
+    {
+        //_mediator = mediator;
+    }
+
+    //[HttpPost]
+    //public async Task<IActionResult> CreateBot(CreateBotCommand command)
+    //{
+    //    var id = await _mediator.Send(command);
+    //    return Ok(id);
+    //}
+
+    [HttpGet]
+    public async Task<IActionResult> GetBots()
+    {
+        //var bots = await _mediator.Send(new GetBotsQuery());
+        //return Ok(bots);
+        return Ok();
+    }
+}
