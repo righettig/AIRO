cd057c462de1da966342d523ee4c508f4b5bd8c2
Giacomo Righetti <righetti.giacomo@live.com>
2024-09-21 18:39:44 +0200

    Added unit tests suite

---
+++ back-end/gateways/airo-enterprise-gateway-api/src/commands/commands.service.spec.ts
@@ -0,0 +1,40 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpService } from '@nestjs/axios';
+import { of } from 'rxjs';
+import { createMockResponse, HttpServiceMock } from 'test/test-utils';
+import { CommandsService } from './commands.service';
+
+describe('CommandsService', () => {
+  let service: CommandsService;
+  let httpService: HttpService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CommandsService,
+        HttpServiceMock
+      ],
+    }).compile();
+
+    service = module.get<CommandsService>(CommandsService);
+    httpService = module.get<HttpService>(HttpService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return all commands', async () => {
+      const commands = [{}];
+      const mockResponse = createMockResponse(commands);
+
+      jest.spyOn(httpService, 'get').mockReturnValue(of(mockResponse));
+
+      const result = await service.getAll();
+
+      expect(result).toEqual(mockResponse.data);
+      expect(httpService.get).toHaveBeenCalledWith(`${service['serviceUrl']}/commands`);
+    });
+  });
+});
