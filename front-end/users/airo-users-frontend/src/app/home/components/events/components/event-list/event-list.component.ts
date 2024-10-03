e386719afe963053723601746642fe7a2a45d6bb
Giacomo Righetti <righetti.giacomo@live.com>
2024-10-03 09:11:23 +0200

    Added rough implementation for subscribe/unsubscribe to events

--- front-end/users/airo-users-frontend/src/app/home/components/events/components/event-list/event-list.component.ts
+++ front-end/users/airo-users-frontend/src/app/home/components/events/components/event-list/event-list.component.ts
@@ -1,13 +1,51 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Event } from '../../models/event.model';
+import { MatSelectModule } from '@angular/material/select';
+import { BotStoreService } from '../../../bots/services/bot-store.service';
+import { Bot } from '../../../bots/models/bot.model';
+import { EventSubscriptionService } from '../../services/event-subscription.service';
 
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
   styleUrl: './event-list.component.scss',
   standalone: true,
-  imports: []
+  imports: [
+    MatSelectModule
+  ]
 })
-export class EventListComponent {
+export class EventListComponent implements OnInit {
   @Input() events: Event[] = [];
+
+  myBots: Bot[] = [];
+  selectedBotId: string | undefined = undefined;
+  subscribing = false;
+  eventId: string | undefined = undefined;
+
+  constructor(
+    private readonly botStoreService: BotStoreService,
+    private readonly eventSubscriptionService: EventSubscriptionService) {}
+
+  async ngOnInit() {
+    this.myBots = await this.botStoreService.getMyBots();
+  }
+
+  subscribe(eventId: string) {
+    this.eventId = eventId;
+    this.subscribing = true;
+  }
+
+  onBotChange(botId: string) {
+    this.selectedBotId = botId;
+  }
+
+  async confirmSubscription() {
+    // show confirmation dialog
+    await this.eventSubscriptionService.subscribeToEvent(this.eventId!, this.selectedBotId!);
+  }
+
+  async unsubscribe(eventId: string) {
+    // show confirmation dialog
+    await this.eventSubscriptionService.unsubscribeFromEvent(eventId);
+  }
 }
---
+++ front-end/users/airo-users-frontend/src/app/home/components/events/services/event-subscription.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { AuthService } from '../../../../auth/services/auth.service';
+import { ConfigService } from '../../../../common/services/config.service';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class EventSubscriptionService {
+    get apiUrl(): string {
+        return `${this.configService.config.gatewayApiUrl}/gateway`;
+    }
+
+    constructor(
+        private authService: AuthService,
+        private configService: ConfigService,
+        private http: HttpClient) { }
+
+
+    async subscribeToEvent(eventId: string, botId: string): Promise<void> {
+        const httpHeaders: HttpHeaders = new HttpHeaders({
+            Authorization: this.authService.accessToken!
+        });
+
+        await firstValueFrom(
+            this.http.post(`${this.apiUrl}/event-subscription`, {
+                eventId,
+                botId
+            }, 
+            { headers: httpHeaders }),
+        );
+    }
+
+    async unsubscribeFromEvent(eventId: string): Promise<void> {
+        const httpHeaders: HttpHeaders = new HttpHeaders({
+            Authorization: this.authService.accessToken!
+        });
+
+        await firstValueFrom(
+            this.http.delete(`${this.apiUrl}/event-subscription`, { 
+                headers: httpHeaders,
+                body: { eventId }
+            }),
+        );
+    }
+}
