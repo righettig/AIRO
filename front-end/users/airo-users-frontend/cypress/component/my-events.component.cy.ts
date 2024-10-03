c1ba74b3e7b58639877a967212c4a4ba47e26ee9
Giacomo Righetti <righetti.giacomo@live.com>
2024-10-03 09:15:54 +0200

    Added placeholder components for my-events

---
+++ front-end/users/airo-users-frontend/cypress/component/my-events.component.cy.ts
@@ -0,0 +1,7 @@
+import { MyEventsComponent } from '../../src/app/home/components/events/components/my-events/my-events.component'
+
+describe('MyEventsComponent', () => {
+  it('should mount', () => {
+    cy.mount(MyEventsComponent)
+  })
+})
--- front-end/users/airo-users-frontend/src/app/home/components/events/events.component.ts
+++ front-end/users/airo-users-frontend/src/app/home/components/events/events.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { EventsService } from './services/events.service';
 import { Event } from './models/event.model';
 import { EventListComponent } from "./components/event-list/event-list.component";
+import { MyEventsComponent } from "./components/my-events/my-events.component";
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrl: './events.component.scss',
   standalone: true,
-  imports: [EventListComponent]
+  imports: [EventListComponent, MyEventsComponent]
 })
 export class EventsComponent implements OnInit {
   events: Event[] = [];
