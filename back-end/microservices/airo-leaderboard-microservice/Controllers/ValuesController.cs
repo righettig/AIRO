8638694b361e3c7a1de55f80cd7323ee25b3dd7f
Giacomo Righetti <righetti.giacomo@live.com>
2024-10-09 18:21:09 +0200

    Added microservice skeleton

---
+++ back-end/microservices/airo-leaderboard-microservice/Controllers/ValuesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace airo_leaderboard_microservice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
