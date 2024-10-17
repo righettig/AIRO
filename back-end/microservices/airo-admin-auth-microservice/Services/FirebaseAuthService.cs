d02e7c2d58d5028ed6f42061b3be661a72ae5aca
Giacomo Righetti <righetti.giacomo@live.com>
2024-10-17 17:19:59 +0200

    Cleanup

--- back-end/microservices/airo-admin-auth-microservice/Services/FirebaseAuthService.cs
+++ back-end/microservices/airo-admin-auth-microservice/Services/FirebaseAuthService.cs
@@ -5,11 +5,9 @@ namespace airo_admin_auth_microservice.Services;
 
 public class FirebaseAuthService(IFirebaseAuthClient firebaseAuth) : IAuthService
 {
-    private readonly IFirebaseAuthClient _firebaseAuth = firebaseAuth;
-
     public async Task<LoginResponse?> Login(string email, string password)
     {
-        var userCredentials = await _firebaseAuth.SignInWithEmailAndPasswordAsync(email, password);
+        var userCredentials = await firebaseAuth.SignInWithEmailAndPasswordAsync(email, password);
 
         if (userCredentials is null)
         {
@@ -23,9 +21,9 @@ public class FirebaseAuthService(IFirebaseAuthClient firebaseAuth) : IAuthServic
 
     public async Task<string?> RefreshToken()
     {
-        var newToken = await _firebaseAuth.User.GetIdTokenAsync(forceRefresh: true);
+        var newToken = await firebaseAuth.User.GetIdTokenAsync(forceRefresh: true);
         return newToken;
     }
 
-    public void SignOut() => _firebaseAuth.SignOut();
+    public void SignOut() => firebaseAuth.SignOut();
 }
