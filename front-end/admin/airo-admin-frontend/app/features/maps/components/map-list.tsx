a37c3877bb3fe548b42c457487b4e8ad9f34efc0
Giacomo Righetti <righetti.giacomo@live.com>
2024-10-24 12:53:42 +0200

    Refactoring, bugfix

--- front-end/admin/airo-admin-frontend/app/features/maps/components/map-list.tsx
+++ front-end/admin/airo-admin-frontend/app/features/maps/components/map-list.tsx
@@ -1,68 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Map from '../types/map';
-import MapDto from '../types/map.dto';
 
 import './map-list.css';
 
-const MapList: React.FC<{ onSelectMap: (map: Map) => void }> = ({ onSelectMap }) => {
-    const [maps, setMaps] = useState<Map[]>([]);
-
-    useEffect(() => {
-        const fetchMaps = async () => {
-            try {
-                const response = await fetch('http://localhost:3001/gateway/maps');
-                const data: MapDto[] = await response.json();
-                const mappedData = data.map(x => {
-                    const mapData = JSON.parse(x.mapData);
-                    return { id: x.id, ...mapData };
-                });
-                setMaps(mappedData);
-
-                // Select the first map by default if available
-                if (mappedData.length > 0) {
-                    onSelectMap(mappedData[0]);
-                }
-            } catch (error) {
-                console.error('Error fetching maps:', error);
-            }
+const MapList: React.FC<
+    {
+        maps: Map[];
+        onSelectMap: (map: Map) => void;
+        onAddNewMap: () => void
+    }> =
+    ({ maps, onSelectMap, onAddNewMap }) => {
+        const truncateName = (name: string) => {
+            return name.length > 20 ? `${name.slice(0, 20)}...` : name;
         };
 
-        fetchMaps();
-    }, []);
-
-    const handleAddNewMap = () => {
-        const untitledCount = maps.filter(map => map.name.startsWith('Untitled')).length + 1;
-
-        const newMap: Map = {
-            id: null,
-            name: `Untitled ${untitledCount}`,
-            size: 8,
-            tiles: []
-        };
-        setMaps([...maps, newMap]);
+        return (
+            <div className="map-list-container">
+                <button className="add-map-button" onClick={onAddNewMap}>Add New Map</button>
+                <ul>
+                    {maps.map(map => (
+                        <li
+                            key={map.id ?? Math.random()} // Add fallback if `id` is null
+                            onClick={() => onSelectMap(map)}
+                            className="map-list-item"
+                            title={map.name}
+                        >
+                            {truncateName(map.name)}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
     };
-
-    const truncateName = (name: string) => {
-        return name.length > 20 ? `${name.slice(0, 20)}...` : name;
-    };
-
-    return (
-        <div className="map-list-container">
-            <button className="add-map-button" onClick={handleAddNewMap}>Add New Map</button>
-            <ul>
-                {maps.map(map => (
-                    <li
-                        key={map.id ?? Math.random()} // Add fallback if `id` is null
-                        onClick={() => onSelectMap(map)}
-                        className="map-list-item"
-                        title={map.name}
-                    >
-                        {truncateName(map.name)}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-};
 
 export default MapList;
