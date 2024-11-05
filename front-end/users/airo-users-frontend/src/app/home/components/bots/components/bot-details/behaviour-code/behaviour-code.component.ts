83cd4265ba2cf0a2f0d2df6b302ddca08b05fd3c
Giacomo Righetti <righetti.giacomo@live.com>
2024-11-05 17:42:13 +0100

    Improved visuals; code cleanup

--- front-end/users/airo-users-frontend/src/app/home/components/bots/components/bot-details/behaviour-code/behaviour-code.component.ts
+++ front-end/users/airo-users-frontend/src/app/home/components/bots/components/bot-details/behaviour-code/behaviour-code.component.ts
@@ -1,34 +1,49 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-behaviour-code',
   template: `
-    <div class="behaviour-container">
-      <div class="title-bar">
-        <div>{{name}} @if (isModified) {<span>*</span>}</div>
-        <div style="display: flex; gap: 4px;">
-          <button (click)="validate.emit()">Validate</button>
-          <button (click)="edit.emit()">Edit</button>
-          <button (click)="delete.emit()">Delete</button>
+    <div class="container">
+      <div class="top-panel">
+        <div class="title-bar">
+          <!-- TODO: it should be possible to rename the script -->
+          <div>{{name}} @if (isModified) {<span>*</span>}</div>
+
+          <div class="title-bar-actions">
+            <!-- TODO: display while validating/saving -->
+            <!-- <mat-spinner [diameter]="16"></mat-spinner> -->
+            <button (click)="validate.emit()">Validate</button>
+            <button (click)="edit.emit()">Edit</button>
+            <button (click)="delete.emit()">Delete</button>
+          </div>
+        </div>
+        <div class="content">
+          @if (isReadOnly) {
+            <pre>{{ code }}</pre>
+          } @else {
+            <textarea [(ngModel)]="code" (ngModelChange)="checkModified()"></textarea>
+          }
         </div>
       </div>
-      <div class="content">
-        @if (isReadOnly) {
-          <pre>{{ code }}</pre>
-        } @else {
-          <textarea [(ngModel)]="code" (ngModelChange)="checkModified()"></textarea>
-        }
-      </div>
-      <!-- <div class="validation-result">
+      <!-- TODO: display after validation/saving when there are errors. 
+       It should be possible to expand/collapse the section -->
+      <!-- <section class="bottom-panel">
+        <div class="bottom-panel-actions">
+          <div>Errors: 0</div>
+          <div>Collapse</div>
+        </div>
+        <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eligendi, pariatur, cupiditate similique alias aperiam reprehenderit fugiat quas vel laudantium fugit dolores, explicabo possimus magni iusto rem blanditiis sit dignissimos.
           Id assumenda voluptate totam cumque, facere qui distinctio reprehenderit expedita voluptates quisquam ab sit consectetur veniam asperiores modi dolor minus nisi inventore placeat veritatis quo. Quia tenetur corrupti accusantium pariatur?
-      </div> -->
+        </p>
+      </section> -->
     </div>
   `,
   styleUrl: './behaviour-code.component.scss',
   standalone: true,
-  imports: [FormsModule]
+  imports: [FormsModule, MatProgressSpinnerModule]
 })
 export class BehaviourCodeComponent {
   @Input() name: string = '';
