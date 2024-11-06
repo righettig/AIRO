7ca6aac59276bcb233805d0150ddf7beac2da8a3
Giacomo Righetti <righetti.giacomo@live.com>
2024-11-06 15:29:06 +0100

    UI improvements

--- front-end/users/airo-users-frontend/src/app/home/components/bots/components/bot-details/behaviour-code/behaviour-code.component.ts
+++ front-end/users/airo-users-frontend/src/app/home/components/bots/components/bot-details/behaviour-code/behaviour-code.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
@@ -8,12 +8,16 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     <div class="container">
       <div class="top-panel">
         <div class="title-bar">
-          <!-- TODO: it should be possible to rename the script -->
-          <div>{{name}} @if (isModified) {<span>*</span>}</div>
-
+          <div>
+            @if (isReadOnly) { 
+              <span>{{ name }}</span> 
+            } @else {
+              <input [(ngModel)]="editableName" (ngModelChange)="onNameChange()" />
+            }
+            @if (isModified) {<span>*</span>}
+          </div>
           <div class="title-bar-actions">
-            <!-- TODO: display while validating/saving -->
-            <!-- <mat-spinner [diameter]="16"></mat-spinner> -->
+            @if (isLoading) { <mat-spinner [diameter]="16"></mat-spinner> }
             <button (click)="validate.emit()">Validate</button>
             <button (click)="edit.emit()">Edit</button>
             <button (click)="delete.emit()">Delete</button>
@@ -27,33 +31,38 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
           }
         </div>
       </div>
-      <!-- TODO: display after validation/saving when there are errors. 
-       It should be possible to expand/collapse the section -->
-      <!-- <section class="bottom-panel">
-        <div class="bottom-panel-actions">
-          <div>Errors: 0</div>
-          <div>Collapse</div>
-        </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eligendi, pariatur, cupiditate similique alias aperiam reprehenderit fugiat quas vel laudantium fugit dolores, explicabo possimus magni iusto rem blanditiis sit dignissimos.
-          Id assumenda voluptate totam cumque, facere qui distinctio reprehenderit expedita voluptates quisquam ab sit consectetur veniam asperiores modi dolor minus nisi inventore placeat veritatis quo. Quia tenetur corrupti accusantium pariatur?
-        </p>
-      </section> -->
+      @if (validationResult?.errors!.length) {
+        <section class="bottom-panel">
+          <div class="bottom-panel-actions">
+            <div>Errors: {{ validationResult!.errors.length }}</div>
+            <button (click)="toggleCollapse()">
+              {{ isCollapsed ? 'Expand' : 'Collapse' }}
+            </button>
+          </div>
+          @if (!isCollapsed) {
+            <div class="validation-content">
+              @for (error of validationResult!.errors; track error) {
+                <p (dblclick)="errorSelected(error)">{{ error }}</p>
+              }
+            </div>
+          }
+        </section>
+      }
     </div>
   `,
   styleUrl: './behaviour-code.component.scss',
   standalone: true,
   imports: [FormsModule, MatProgressSpinnerModule]
 })
-export class BehaviourCodeComponent {
+export class BehaviourCodeComponent implements OnInit {
   @Input() name: string = '';
   @Input() isReadOnly: boolean = false;
+  @Input() isLoading: boolean = false; 
+  @Input() validationResult: { errors: string[] } | undefined = undefined;
 
   private _initialCode: string = '';
   private _code: string = '';
   
-  isModified: boolean = false;  // Track modification status
-
   @Input()
   set code(value: string) {
     this._code = value;
@@ -71,10 +80,32 @@ export class BehaviourCodeComponent {
   @Output() validate = new EventEmitter<void>();
   @Output() edit = new EventEmitter<void>();
   @Output() delete = new EventEmitter<void>();
+  @Output() nameChange = new EventEmitter<string>();
   
+  isModified: boolean = false;  // Track modification status
+  isCollapsed: boolean = true;  // Show/hide the validation errors panel
+
+  editableName: string = '';
+
+  ngOnInit(): void {
+    this.editableName = this.name;
+  }
+
+  onNameChange() {
+    this.nameChange.emit(this.editableName);  // Emit the new name when it changes
+  }
+
   checkModified(): void {
     this.isModified = this._code !== this._initialCode;
     this.modified.emit(this.isModified);  // Emit true if modified, false otherwise
     this.codeChange.emit(this._code);
+  }
+
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  errorSelected(error: string) {
+    alert(error);
   }
 }
